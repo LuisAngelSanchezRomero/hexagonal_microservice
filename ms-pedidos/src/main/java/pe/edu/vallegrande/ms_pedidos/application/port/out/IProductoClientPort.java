cebdac7c829b2ff7261124e0ffcca7bf794b20dd
LuisSánchez <luis.sanchez.romero@vallegrande.edu.pe>
2026-06-26 17:48:00 +0000

    fix: correct project configuration

--- ms-pedidos/src/main/java/pe/edu/vallegrande/ms_pedidos/application/port/out/IProductoClientPort.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.ms_pedidos.application.port.out;
-
-import pe.edu.vallegrande.ms_pedidos.domain.model.Producto;
-import reactor.core.publisher.Mono;
-
-public interface IProductoClientPort {
-
-    Mono<Producto> findById(Long id);
-}
--- ms-productos/ms-pedidos/src/main/java/pe/edu/vallegrande/mspedidos/infrastructure/adapter/out/persistency/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.vallegrande.mspedidos.infrastructure.adapter.out.persistency;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import pe.edu.vallegrande.mspedidos.domain.model.Pedido;
-
-public interface PedidoRepository extends ReactiveCrudRepository<Pedido, Long> {
-}
